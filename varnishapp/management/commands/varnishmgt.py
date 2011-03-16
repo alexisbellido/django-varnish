3b5d9f7f7c61e84c8e0308262f5e0af5a08a4b87
Kenneth Falck <kennu@iki.fi>
2011-03-16 15:45:52 +0200

    Added missing secret arg

--- varnishapp/management/commands/varnishmgt.py
+++ varnishapp/management/commands/varnishmgt.py
@@ -1,10 +1,11 @@
 from django.core.management.base import BaseCommand
+from django.conf import settings
 from varnishapp.manager import manager
 from pprint import pprint
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         if args:
-            pprint(manager.run(*args))
+            pprint(manager.run(*args, secret=getattr(settings, 'VARNISH_SECRET', None)))
         else:
             print manager.help()
